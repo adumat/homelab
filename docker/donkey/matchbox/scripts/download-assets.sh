dd5bd9e89690382821c3aaa742e8e390e2bb525a
adumat <matteo.a91@gmail.com>
2026-02-14 15:47:58 +0100

    fix(matchbox): source Talos version from talenv.yaml instead of hardcoding

--- docker/donkey/matchbox/scripts/download-assets.sh
+++ docker/donkey/matchbox/scripts/download-assets.sh
@@ -3,8 +3,7 @@ set -euo pipefail
 
 # https://factory.talos.dev/?arch=amd64&bootloader=auto&cmdline-set=true&extensions=-&extensions=siderolabs%2Fi915&extensions=siderolabs%2Fintel-ucode&extensions=siderolabs%2Fiscsi-tools&extensions=siderolabs%2Fmei&extensions=siderolabs%2Fnfsrahead&extensions=siderolabs%2Fnut-client&extensions=siderolabs%2Fnvme-cli&extensions=siderolabs%2Futil-linux-tools&platform=metal&target=metal&version=1.12.1
 SCHEMATIC_ID="26034a8501cbcbc2a9788b3e5b74c04ceae8637e07b4a8075a1f41f5e0579615"
-# renovate: datasource=docker depName=ghcr.io/siderolabs/installer
-VERSION="v1.12.1"
+VERSION=$(yq -e '.talosVersion' /work/talos/talenv.yaml)
 
 ASSETS_DIR="/assets"
 
